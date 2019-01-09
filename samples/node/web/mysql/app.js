046db0b3093060b1694ba6fe796ead8456c7a633
ALEXKINGS <2778896718@qq.com>
2019-01-09 11:01:53 +0800

    Adjust the directory（调整目录）

---
+++ samples/node/web/mysql/app.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+const config = require("./config");
+
+var sequelize = new Sequelize(config.database, config.username, config.password,{
+	host: config.host,
+	dialect: "mysql",
+	pool: {
+		max: 5,
+		min: 0,
+		idle:30000
+	}
+});
+
+
+var Pet = sequelize.define("pet", {
+	id: {
+		type: Sequelize.STRING(50),
+		primaryKey: true
+	},
+	name: Sequelize.STRING(100),
+	gender: Sequelize.BOOLEAN,
+	birth: Sequelize.STRING(10),
+	createdAt: Sequelize.BIGINT,
+	updatedAt: Sequelize.BIGINT,
+	version: Sequelize.BIGINT
+}, {
+	timestamps: false
+})
